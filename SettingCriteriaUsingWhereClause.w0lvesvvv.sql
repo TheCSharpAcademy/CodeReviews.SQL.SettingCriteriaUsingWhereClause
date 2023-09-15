3a11df33cd4b0f7b0f3d8e6a6c2327d39c807925
Pablo Martinez Buesa <rekshooter@gmail.com>
2023-09-15 17:54:41 +0200

    Fixed with the new requirements

--- SettingCriteriaUsingWhereClause.w0lvesvvv.sql
+++ SettingCriteriaUsingWhereClause.w0lvesvvv.sql
@@ -1,9 +1,9 @@
--- Write a query to list out all of the events from the tblEvent table in category number 11 (which corresponds to Love and Relationships, as it happens)
+-- List out all of the events from the tblEvent table in category number 11 (which corresponds to Love and Relationships, as it happens)
 SELECT * 
 FROM dbo.tblEvent
 WHERE CategoryID = 11
 
--- Create a query which lists out all of the tblEvent events which include the words Teletubbies OR Pandy.
+-- List out all of the tblEvent events which include the words Teletubbies OR Pandy.
 SELECT *
 FROM dbo.tblEvent
 WHERE EventName LIKE '%Teletubbies%' 
@@ -11,28 +11,48 @@ WHERE EventName LIKE '%Teletubbies%'
 	OR EventDetails LIKE '%Teletubbies%' 
 	OR EventDetails LIKE '%Pandy%'
 
--- Create a query which lists out all of the events which took place in February 2005.
+-- List out all of the events which took place in February 2005.
 SELECT *
 FROM dbo.tblEvent
 WHERE EventDate >= '2005-02-01 00:00:00'
 	AND EventDate <= '2005-03-01 00:00:00'
-	-- Can be used between but i don't like it :)
-	-- AND EventDate BETWEEN '2005-02-01 00:00:00' AND '2005-03-01 00:00:00'
 
--- Write a query with the following FOUR criteria: 
--- events that take place in one of the island countries (8, 22, 30 and 35, corresponding to Japan, the Marshall Islands, Cuba and Sri Lanka respectively); 
--- their EventDetails column contains the word Water (not the text Water, but the word); 
--- Their category is number 4 (Natural World). 
--- They happened in 1970 or after.
-SELECT *
+-- Show a list of all events which might have something to do with water. 
+-- ONE OR MORE of the following is true: 
+--		events that take place in one of the island countries (8, 22, 30 and 35, corresponding to Japan, the Marshall Islands, Cuba and Sri Lanka respectively); 
+--		their EventDetails column contains the word Water (not the text Water, but the word); 
+--		Their category is number 4 (Natural World). 
+-- This list should return 11 rows. 
+SELECT * 
 FROM dbo.tblEvent
 WHERE CountryID IN (8, 22, 30, 35)
-	AND EventDetails LIKE '% water %'
-	AND CategoryID = 4
-	AND EventDate >= '1970-01-01 00:00:00'
+	OR EventDetails LIKE '% water %'
+	OR CategoryID = 4
+-- Then add a criterion to show only those events which happened since 1970 (5 rows).
+SELECT * 
+FROM dbo.tblEvent
+WHERE EventDate >= '1970-01-01 00:00:00'
+	AND (CountryID IN (8, 22, 30, 35)
+		OR EventDetails LIKE '% water %'
+		OR CategoryID = 4)
 
 -- Create a query with events which aren't in the Transport category (number 14), but which nevertheless include the text Train in the EventDetails column.
 SELECT *
 FROM dbo.tblEvent
 WHERE CategoryID != 14
-	AND EventDetails NOT LIKE ('%Train%')
+	AND EventDetails LIKE '%train%'
+
+-- List events which are in the Space country (number 13), but which don't mention Space in either the event name or the event details columns.
+SELECT *
+FROM dbo.tblEvent
+WHERE CountryID = 13
+	AND EventName NOT LIKE '%space%'
+	AND EventDetails NOT LIKE '%space%'
+	
+
+-- Events which are in categories 5 or 6 (War/conflict and Death/disaster), but which don't mention either War or Death in the EventDetails column.
+SELECT *
+FROM DBO.tblEvent
+WHERE CategoryID IN (5, 6)
+	AND EventDetails NOT LIKE '%war%'
+	AND EventDetails NOT LIKE '%death%'
